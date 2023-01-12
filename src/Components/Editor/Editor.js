8416a48031260f27044fa5fcea933058f03d4f58
benni maxon <benni.maxon@gmail.com>
2023-01-12 11:15:53 -0800

    built first selector in editor

---
+++ src/Components/Editor/Editor.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Editor.css';
+
+export default function Editor() {
+  return (
+    <div className="editor">
+      <div>
+        <select>
+          <option value="aries">Aries</option>
+          <option value="taurus">Taurus</option>
+          <option value="gemini">Gemini</option>
+          <option value="cancer">Cancer</option>
+          <option value="leo">Leo</option>
+          <option value="virgo">Virgo</option>
+          <option value="libra">Libra</option>
+          <option value="scorpio">Scorpio</option>
+          <option value="sagittarius">Sagittarius</option>
+          <option value="capricorn">Capricorn</option>
+          <option value="aquarius">Aquarius</option>
+          <option value="pisces">Pisces</option>
+        </select>
+      </div>
+    </div>
+  );
+}
