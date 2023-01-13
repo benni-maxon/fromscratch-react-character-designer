86c4f1f0725b9c921e9db81e5b07c4cd45e588b7
benni maxon <benni.maxon@gmail.com>
2023-01-13 14:45:20 -0800

    more styling

--- src/Components/Display/Display.js
+++ src/Components/Display/Display.js
@@ -12,16 +12,17 @@ export default function Display({
   affirmations,
 }) {
   return (
-    <div>
+    <div className="signs-and-stats-div">
       <div className="signs-div">
         <img alt={sun} src={`${process.env.PUBLIC_URL}/signs/${sun}.png`} />
         <img alt={moon} src={`${process.env.PUBLIC_URL}/signs/${moon}.png`} />
         <img alt={rising} src={`${process.env.PUBLIC_URL}/signs/${rising}.png`} />
       </div>
       <div className="stats-div">
-        <p>
-          You have changed your Sun Sign {sunCount} times, your Moon Sign {moonCount} times, and
-          your Rising Sign {risingCount} times.
+        <p className="stats-p">
+          You have changed your Sun Sign {sunCount} time(s), <br />
+          your Moon Sign {moonCount} time(s), <br />
+          and your Rising Sign {risingCount} time(s).
         </p>
         <h2>Your Affirmations:</h2>
         <ul>
